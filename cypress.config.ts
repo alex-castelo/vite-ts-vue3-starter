28642c1b9af7ab1a488973d1f4391ca18f3d07c8
Àlex Castelo Martínez <alexcastelomartinez@gmail.com>
2024-02-22 16:53:42 +0100

    add cucumber pre-processor plugin

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'cypress'
 
+import { cucumberPlugin } from './cypress/plugins/cucumber'
+
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000/',
+    setupNodeEvents: cucumberPlugin,
+    specPattern: '**/*.feature',
   },
 })
